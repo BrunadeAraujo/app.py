60c7b15b1a2ff79c04bd68b0f297c0013605e6a7
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-10-06 08:52:38 -0300

    def

--- app.py
+++ app.py
@@ -22,19 +22,33 @@ def finalizar_app():
     print('Finalizando o app')
 
 def escolher_opcao():
+   try:
     opcao_escolhida = int(input('Escolha uma opção: '))
 
     if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
+       cadastrar_novo_restaurante()
     elif opcao_escolhida == 2:
         print('Listar restaurantes')
     elif opcao_escolhida == 3:
         print('Ativar restaurantes')
-    else:
+    elif opcao_escolhida == 4:
         finalizar_app()
+    else:
+       opcao_invalida()
+   except:
+    opcao_invalida()
 
+
+def opcao_invalida():
+    print('Opção inválida')
+    input('Digite uma tecla para voltar ao Menu Principal\n')
+    main()
+
+def cadastrar_novo_restaurante():
+    pass
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
@@ -43,4 +57,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-
